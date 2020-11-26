449d45f3e844d7fd13836739217199579eb3df20
Panjkrc <panjkrc.mihael@gmail.com>
2020-11-26 15:28:55 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -9,7 +9,6 @@ module.exports = {
 		const search = Comic.find({$text: {$search: query}})
 		
 		search.exec().then((comics) => {
-			console.log(comics)
 			const amount = comics.length
 			const comic = comics[0]
 			comment.reply(`${comic.title} #${comic.num} <br> ![](${comic.img}) <hr> <sub>Your search returned ${amount} comics, and I'm ${((1/amount)*100).toRound(2)}% sure you searched for this comic`)
